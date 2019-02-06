6b4b7665ce5436acdba28d4c3d42297ac6cc6bd6
Pedro Salazar <pedrosalazard@gmail.com>
2019-02-05 22:16:37 -0500

    Cambió de config para ejecutar OpenMP

--- cuda/secYang_mp.c
+++ cuda/secYang_mp.c
@@ -21,7 +21,7 @@ int buscarIndice(char *cadena, char a);
 int main(void)
 {
     int thread_count = omp_get_max_threads();
-    //for(int thread_count= 1 ; thread_count <=32 ; thread_count = thread_count * 2 ){
+    for(int thread_count= 1 ; thread_count <=32 ; thread_count = thread_count * 2 ){
         for (int i = 2; i <= TAM_MAX;){
             double begin = omp_get_wtime();
             //char *a = "ABMDEBMA";
@@ -30,8 +30,8 @@ int main(void)
             char *b = rand_string_alloc(i);
             int m = strlen(a);
             int n = strlen(b);
-            //lcs_openmp(a, b, m, n, thread_count);
-            lcs(a, b, m, n);
+            lcs_openmp(a, b, m, n, thread_count);
+            //lcs(a, b, m, n);
             free(a);
             free(b);
 
@@ -47,7 +47,7 @@ int main(void)
                     i = 5000;
             }
         }
-    //}
+    }
 /*
     // Comparación entre los dos algoritmos
         int i = 40000;
