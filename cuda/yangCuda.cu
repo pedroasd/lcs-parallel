6e7aadc9b885bec634fb6a06383ed6eac4114b01
Pedro Salazar <pedrosalazard@gmail.com>
2019-02-09 07:55:51 -0500

    Ajuste para toma de estadísticas

--- cuda/yangCuda.cu
+++ cuda/yangCuda.cu
@@ -71,8 +71,8 @@ int main(int argc, char *argv[])
         for(int thread_count= block_count; thread_count <= NUMTHREADS; thread_count = thread_count * 2 ){
             //for (int i = 2; i <= TAM;){
 
-                int threadsPerBlock = thread_count/block_count;
-                if(threadsPerBlock <= MAX_THREADS_BLOCK){
+                //int threadsPerBlock = thread_count/block_count;
+                //if(threadsPerBlock <= MAX_THREADS_BLOCK){
 
                     double begin = omp_get_wtime();
                     //char *a = "ABMDEBMA";
@@ -91,7 +91,7 @@ int main(int argc, char *argv[])
                     double time_spent = end - begin;
                     
                     printf("B;%d;N;%d;I;%d;T;%f\n", block_count,thread_count, i, time_spent);
-                }
+                //}
 
                 /*if (i > 2048)
                     i += 5000;
