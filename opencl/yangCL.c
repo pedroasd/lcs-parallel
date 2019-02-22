28296d75a5c3f35580f0aad2f33d5d502c388b2d
Pedro Salazar <pedrosalazard@gmail.com>
2019-02-21 21:50:07 -0500

    Generar estadísticas

--- opencl/yangCL.c
+++ opencl/yangCL.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
     //int block_count = BLOCKS;
     //int thread_count = NUMTHREADS;
     //int thread_count = TAM/2;
-    for(int block_count = 100; block_count <= BLOCKS; block_count=block_count+50){
+    for(int block_count = 150; block_count <= BLOCKS; block_count=block_count+50){
         if(block_count == 52) block_count = 50;
         for(int thread_count= block_count; thread_count <= NUMTHREADS; thread_count=thread_count+100 ){
             //for (int i = 2; i <= TAM;){
